ce88484bdf6a46f71f4b61faba19f26bc5dd157b
Evgeny Persienko <e.persienko@office.ngs.ru>
2014-09-25 13:53:43 +0700

    Placed io tests to separated file

---
+++ io_test.go
@@ -0,0 +1,148 @@
+package zhash
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestReadHashSuccess(t *testing.T) {
+	fd, err := os.Open("test.json")
+	if err != nil {
+		t.Error("Cannot read test.json!")
+	}
+	defer fd.Close()
+
+	h := NewHash(nil, json.Unmarshal)
+	err = h.ReadHash(fd)
+	if err != nil {
+		t.Error("Error reading hash!")
+	}
+}
+
+func TestReadHashFailParse(t *testing.T) {
+	fd, err := os.Open("test_corrupted.json")
+	if err != nil {
+		t.Error("Cannot read test_corrupted.json!")
+	}
+	defer fd.Close()
+
+	h := NewHash(nil, json.Unmarshal)
+	err = h.ReadHash(fd)
+	if err == nil {
+		t.Error("Hash readed from corrupted source!")
+	}
+}
+
+func TestReadHashFailNoUnmarshaller(t *testing.T) {
+	fd, err := os.Open("test_corrupted.json")
+	if err != nil {
+		t.Error("Cannot read test_corrupted.json!")
+	}
+	defer fd.Close()
+
+	h := NewHash(nil, nil)
+	err = h.ReadHash(fd)
+	if err == nil {
+		t.Error("Hash readed from corrupted source!")
+	}
+}
+
+type corruptedReader string
+
+func (c corruptedReader) Read(b []byte) (int, error) {
+	return 0, errors.New("I'm corrupted!")
+}
+
+func TestReadHashFailReaderErr(t *testing.T) {
+	r := corruptedReader("aaA")
+
+	h := NewHash(nil, nil)
+	h.SetUnmarshallerFunc(json.Unmarshal)
+	err := h.ReadHash(r)
+	if err == nil {
+		t.Error("Hash readed from corrupted source!")
+	}
+}
+
+func TestHashReader(t *testing.T) {
+	conf := HashFromMap(testMap, nil, nil)
+	r := conf.Reader()
+	f, err := os.OpenFile(os.DevNull, os.O_RDWR, 0666)
+	if err != nil {
+		t.Error("Error opening DevNull:", err)
+	}
+	defer f.Close()
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		t.Error("Error reading from conf.Reader():", err)
+	}
+	f.Write(buf)
+}
+
+func TestToStringSuccess(t *testing.T) {
+	conf := HashFromMap(testMap, nil, nil)
+
+	t.Logf("Hash: %s", conf)
+}
+
+type buggyStruct struct {
+	Id int `json:"id"`
+}
+
+func (b buggyStruct) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("Baka!")
+}
+
+func TestToStringFail(t *testing.T) {
+	conf := NewHash(nil, nil)
+
+	value := buggyStruct{Id: 10}
+	conf.Set(value, "meta", "bug")
+
+	t.Logf("Hash: %s", conf)
+}
+
+func TestToJson(t *testing.T) {
+	hash := HashFromMap(map[string]interface{}{
+		"rec1": "val one",
+		"rec2": map[string]interface{}{
+			"sub_rec1": 2,
+			"sub_rec2": "string",
+		},
+	}, json.Marshal, json.Unmarshal)
+
+	jsonText := "{\"rec1\":\"val one\",\"rec2\":{\"sub_rec1\":2,\"sub_rec2\":\"string\"}}"
+
+	convert, err := json.Marshal(hash)
+	if err != nil {
+		t.Error("Error marshalling hash to json:", err)
+	}
+
+	if string(convert) != jsonText {
+		t.Errorf("Marshalled json differs from wanted:\nWant: %s\nGot: %s",
+			jsonText, string(convert))
+	}
+}
+
+func TestWriteHash(t *testing.T) {
+	conf := HashFromMap(testMap, nil, nil)
+	conf.SetMarshallerFunc(json.Marshal)
+	f, _ := os.OpenFile(os.DevNull, os.O_RDWR, 0666)
+	defer f.Close()
+	if err := conf.WriteHash(f); err != nil {
+		t.Errorf("Errors while writing config: %s", err.Error())
+	}
+}
+
+func TestWriteHashError(t *testing.T) {
+	conf := NewHash(json.Marshal, nil)
+	conf.Set(buggyStruct{10}, "bug")
+	f, _ := os.OpenFile(os.DevNull, os.O_RDWR, 0666)
+	defer f.Close()
+	if err := conf.WriteHash(f); err == nil {
+		t.Errorf("No error while marshalling buggyStruct!")
+	}
+}
--- zhash_test.go
+++ zhash_test.go
@@ -1,10 +1,6 @@
 package zhash
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
-	"os"
 	"reflect"
 	"strings"
 	"testing"
@@ -27,65 +23,6 @@ var testMap = map[string]interface{}{
 		"val1": 10,
 		"val2": 11.0,
 	},
-}
-
-func TestReadHashSuccess(t *testing.T) {
-	fd, err := os.Open("test.json")
-	if err != nil {
-		t.Error("Cannot read test.json!")
-	}
-	defer fd.Close()
-
-	h := NewHash(nil, json.Unmarshal)
-	err = h.ReadHash(fd)
-	if err != nil {
-		t.Error("Error reading hash!")
-	}
-}
-
-func TestReadHashFailParse(t *testing.T) {
-	fd, err := os.Open("test_corrupted.json")
-	if err != nil {
-		t.Error("Cannot read test_corrupted.json!")
-	}
-	defer fd.Close()
-
-	h := NewHash(nil, json.Unmarshal)
-	err = h.ReadHash(fd)
-	if err == nil {
-		t.Error("Hash readed from corrupted source!")
-	}
-}
-
-func TestReadHashFailNoUnmarshaller(t *testing.T) {
-	fd, err := os.Open("test_corrupted.json")
-	if err != nil {
-		t.Error("Cannot read test_corrupted.json!")
-	}
-	defer fd.Close()
-
-	h := NewHash(nil, nil)
-	err = h.ReadHash(fd)
-	if err == nil {
-		t.Error("Hash readed from corrupted source!")
-	}
-}
-
-type corruptedReader string
-
-func (c corruptedReader) Read(b []byte) (int, error) {
-	return 0, errors.New("I'm corrupted!")
-}
-
-func TestReadHashFailReaderErr(t *testing.T) {
-	r := corruptedReader("aaA")
-
-	h := NewHash(nil, nil)
-	h.SetUnmarshallerFunc(json.Unmarshal)
-	err := h.ReadHash(r)
-	if err == nil {
-		t.Error("Hash readed from corrupted source!")
-	}
 }
 
 var setTests = []struct {
@@ -111,21 +48,6 @@ func TestSetPath(t *testing.T) {
 			t.Errorf("#%d: conf[%s]%#v != %#v", i, test.path, val, test.value)
 		}
 	}
-}
-
-func TestHashReader(t *testing.T) {
-	conf := HashFromMap(testMap, nil, nil)
-	r := conf.Reader()
-	f, err := os.OpenFile(os.DevNull, os.O_RDWR, 0666)
-	if err != nil {
-		t.Error("Error opening DevNull:", err)
-	}
-	defer f.Close()
-	buf, err := ioutil.ReadAll(r)
-	if err != nil {
-		t.Error("Error reading from conf.Reader():", err)
-	}
-	f.Write(buf)
 }
 
 type TestGet struct {
@@ -434,70 +356,5 @@ func TestGetBoolFail(t *testing.T) {
 		if err == nil {
 			t.Errorf("#%d: GetBool(%s) doesn't return any error, but it should fail", i, test.path)
 		}
-	}
-}
-
-func TestToStringSuccess(t *testing.T) {
-	conf := HashFromMap(testMap, nil, nil)
-
-	t.Logf("Hash: %s", conf)
-}
-
-type buggyStruct struct {
-	Id int `json:"id"`
-}
-
-func (b buggyStruct) MarshalJSON() ([]byte, error) {
-	return nil, errors.New("Baka!")
-}
-
-func TestToStringFail(t *testing.T) {
-	conf := NewHash(nil, nil)
-
-	value := buggyStruct{Id: 10}
-	conf.Set(value, "meta", "bug")
-
-	t.Logf("Hash: %s", conf)
-}
-
-func TestToJson(t *testing.T) {
-	hash := HashFromMap(map[string]interface{}{
-		"rec1": "val one",
-		"rec2": map[string]interface{}{
-			"sub_rec1": 2,
-			"sub_rec2": "string",
-		},
-	}, json.Marshal, json.Unmarshal)
-
-	jsonText := "{\"rec1\":\"val one\",\"rec2\":{\"sub_rec1\":2,\"sub_rec2\":\"string\"}}"
-
-	convert, err := json.Marshal(hash)
-	if err != nil {
-		t.Error("Error marshalling hash to json:", err)
-	}
-
-	if string(convert) != jsonText {
-		t.Errorf("Marshalled json differs from wanted:\nWant: %s\nGot: %s",
-			jsonText, string(convert))
-	}
-}
-
-func TestWriteHash(t *testing.T) {
-	conf := HashFromMap(testMap, nil, nil)
-	conf.SetMarshallerFunc(json.Marshal)
-	f, _ := os.OpenFile(os.DevNull, os.O_RDWR, 0666)
-	defer f.Close()
-	if err := conf.WriteHash(f); err != nil {
-		t.Errorf("Errors while writing config: %s", err.Error())
-	}
-}
-
-func TestWriteHashError(t *testing.T) {
-	conf := NewHash(json.Marshal, nil)
-	conf.Set(buggyStruct{10}, "bug")
-	f, _ := os.OpenFile(os.DevNull, os.O_RDWR, 0666)
-	defer f.Close()
-	if err := conf.WriteHash(f); err == nil {
-		t.Errorf("No error while marshalling buggyStruct!")
 	}
 }
